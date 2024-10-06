ba997bfe1e158e9db826e2a48262638f8636d381
Your Name <you@example.com>
2024-10-06 11:41:26 +0300

    v1 slid

---
+++ website/src/Pages/Home/Home.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import HeroSection from "../../Components/HeroSection/HeroSection";
+
+export default function Home() {
+    
+    return (
+        <Wrapper className="main_home">
+            <div className="home">
+                <HeroSection />
+                <div className="main_title">
+                    <h2>مؤسسة طريق التعمير للمقاولاتة
+                        <br />
+                        المعمارية العامة
+                    </h2>
+                    <p className="descrition">نبدع ، فنؤثر <span style={{color: "yellow"}}>فنحقق</span> نتائج </p>
+                    
+                </div>
+            </div>
+        </Wrapper>
+
+    );
+};
+
+const Wrapper = styled.section`
+    background-color: ${({theme}) => theme.colors.bg};
+    height: 100vh;
+    position: relative;
+
+    .main_title {
+        position: absolute;
+        top: 28%;
+        right: 34px;
+        direction: rtl;
+
+        h2 {
+            font-weight: bold
+        
+            .descrition {
+                font-size: 32px;
+                color: currentColor;
+                font-weight: bold;
+            }
+        }
+    }
+`;
