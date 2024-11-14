9d0142d71b0b7f3645c9623f64c21a17718c0d25
mahmoud-elmosallmy <mosallmym@gmail.com>
2024-11-14 11:28:09 +0300

    add Carousel

--- src/Components/Carousels/CarouselsSlide.js
+++ src/Components/Carousels/CarouselsSlide.js
@@ -4,7 +4,7 @@ export default function CarouselsSlide() {
   return (
     <MDBCarousel showIndicators showControls fade>
       <MDBCarouselItem itemId={1}>
-        <img src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg' className='d-block w-100' alt='...' />
+        <img src='https://i.pinimg.com/736x/47/7a/9a/477a9a12e5e4cafc38719f5e7bf6be35.jpg' className='d-block w-100' alt='...' style={{height: '568px'}} />
         <MDBCarouselCaption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -12,7 +12,7 @@ export default function CarouselsSlide() {
       </MDBCarouselItem>
 
       <MDBCarouselItem itemId={2}>
-        <img src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg' className='d-block w-100' alt='...' />
+        <img src='https://i.pinimg.com/736x/ed/78/d2/ed78d2c755fad72100208ed70433ba8a.jpg' className='d-block w-100' alt='...' style={{height: '568px'}} />
         <MDBCarouselCaption>
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -20,7 +20,21 @@ export default function CarouselsSlide() {
       </MDBCarouselItem>
 
       <MDBCarouselItem itemId={3}>
-        <img src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg' className='d-block w-100' alt='...' />
+        <img src='https://i.pinimg.com/736x/af/18/cd/af18cd8b07971016c2622b0390886a33.jpg' className='d-block w-100' alt='...' style={{height: '568px'}} />
+        <MDBCarouselCaption>
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </MDBCarouselCaption>
+      </MDBCarouselItem>
+      <MDBCarouselItem itemId={4}>
+        <img src='https://i.pinimg.com/736x/78/3b/2f/783b2fe5e7b2d97149b4685f29ebd425.jpg' className='d-block w-100' alt='...' style={{height: '568px'}} />
+        <MDBCarouselCaption>
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </MDBCarouselCaption>
+      </MDBCarouselItem>
+      <MDBCarouselItem itemId={4}>
+        <img src='https://i.pinimg.com/736x/0e/f5/ef/0ef5ef013d7653a0a3a5d1bee6edf0a5.jpg' className='d-block w-100' alt='...' style={{height: '568px'}} />
         <MDBCarouselCaption>
           <h5>Third slide label</h5>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
